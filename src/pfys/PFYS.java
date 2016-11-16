622165d1bd20a93df6bc099cf252c97eacd0443b
MerijnM <merijnmolenaar@outlook.com>
2016-11-16 14:08:07 +0100

    Textboxes toegevoegd

--- src/pfys/PFYS.java
+++ src/pfys/PFYS.java
@@ -10,6 +10,8 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
@@ -28,14 +30,22 @@ public class PFYS extends Application {
         Text username = new Text();                 //text die gebruikersnaam print bij inlogscherm
         username.setText("Username");
         username.setFont(Font.font("Verdana", 20));
-        username.setFill(Color.BLACK);  
-        username.setTranslateY(-110);   
-        
+        username.setFill(Color.BLACK);
+        username.setTranslateY(-110);
+
+        TextField UsernameText = new TextField();
+        UsernameText.setMaxWidth(220);                           //invoer voor username
+        UsernameText.setTranslateY(-70);
+
+        TextField PasswordText = new TextField();
+        PasswordText.setMaxWidth(220);                        //invoer voor wachtwoord
+        PasswordText.setTranslateY(20);
+
         Text password = new Text();
         password.setText("Password");
         password.setFont(Font.font("Verdana", 20));
-        password.setFill(Color.BLACK);  
-        password.setTranslateY(-20);                                                           
+        password.setFill(Color.BLACK);
+        password.setTranslateY(-20);
 
         Button btn = new Button();
         btn.setText("Login");                                           //inlog button
@@ -46,17 +56,18 @@ public class PFYS extends Application {
 
             @Override
             public void handle(ActionEvent event) {
-                
+
             }
         });
 
         Pane root = new StackPane();
         root.getChildren().add(btn);                        //toevoegen button
         root.getChildren().add(username);                    //toevoegen username text
-        root.getChildren().add(password);                    //toevoegen password text
-        
-        
-        
+        root.getChildren().add(password);
+
+        root.getChildren().add(UsernameText);  //toevoegen Username text
+        root.getChildren().add(PasswordText);  //toevoegen password text
+
         Scene scene = new Scene(root, 300, 250);
 
         primaryStage.setTitle("Applicatie naam");
